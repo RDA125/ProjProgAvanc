50907f6de0979a2920ea17000d40e80f9b756dba
RDA <ricardo.dias.andrade@gmail.com>
2022-06-01 19:19:45 +0100

    Testing to see if i'm able to insert combine game and store into Game_store table: success

--- app/src/androidTest/java/com/example/projprogavanc/TestDataBase.kt
+++ app/src/androidTest/java/com/example/projprogavanc/TestDataBase.kt
@@ -4,7 +4,6 @@ import android.database.sqlite.SQLiteDatabase
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.projprogavanc.DB.*
-import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,31 +20,43 @@ import org.junit.Before
 class TestDataBase {
     fun appContext() = InstrumentationRegistry.getInstrumentation().targetContext
 
-    private fun GetWritableDB(): SQLiteDatabase {
+    /**
+     *
+     * função que devolve a base de dados para escrever o conteudo necessário
+     */
+    private fun getWritableDB(): SQLiteDatabase {
         val openHelper = DBOpenHelper(appContext())
         return openHelper.writableDatabase
 
     }
 
-    private fun InsertGame(db: SQLiteDatabase, game: Game) {
+    /**
+     * Funções para inserir Game, Store e Game_Store
+     */
+    private fun insertGame(db: SQLiteDatabase, game: Game) {
         game.id = TDBGames(db).insert(game.toContentValues())
         assertNotEquals(-1, game.id)
     }
 
-    private fun InsertStore(db: SQLiteDatabase, store: Store) {
+    private fun insertStore(db: SQLiteDatabase, store: Store) {
         store.id = TDBStores(db).insert(store.toContentValues())
         assertNotEquals(-1, store.id)
     }
 
+    private fun insertGameStore(db: SQLiteDatabase, gameStore: Game_Store) {
+        gameStore.id = TDBGame_Store(db).insert(gameStore.toContentValues())
+        assertNotEquals(-1, gameStore.id)
+    }
+
     @Before
-    fun DeleteDataBase(){
+    fun deleteDataBase(){
 
         appContext().deleteDatabase(DBOpenHelper.NAME)
 
     }
 
     @Test
-    fun OpenDB_Test(){
+    fun openDB_Test(){
 
         val openHelper = DBOpenHelper(appContext())
         val db = openHelper.readableDatabase
@@ -55,20 +66,41 @@ class TestDataBase {
 
     }
 
+    /**
+     *
+     * Secção de Testes para verificar se é possivel inserir Game, Store e Game_store
+     *
+     */
     @Test
-    fun InsertGame(){
-        val db = GetWritableDB()
+    fun insertGameTest(){
+        val db = getWritableDB()
 
-        InsertGame(db, Game("Doom","Digital"))
+        insertGame(db, Game("Doom","Digital"))
 
         db.close()
     }
 
     @Test
-    fun InsertStore(){
-        val db = GetWritableDB()
+    fun insertStoreTest(){
+        val db = getWritableDB()
 
-        InsertStore(db, Store("Steam","N/A","Digital"))
+        insertStore(db, Store("Steam","N/A","Digital"))
+
+        db.close()
+    }
+
+    @Test
+    fun insertGame_StoreTest(){
+        val db = getWritableDB()
+
+        val game = Game("Bioshock","Digital")
+        insertGame(db, game)
+
+        val store = Store("Epic Games","N/A","Digital")
+        insertStore(db, store)
+
+        val gameStore = Game_Store(29.99, game.id, store.id)
+        insertGameStore(db,gameStore)
 
         db.close()
     }
--- app/src/main/java/com/example/projprogavanc/DB/Game_Store.kt
+++ app/src/main/java/com/example/projprogavanc/DB/Game_Store.kt
@@ -2,12 +2,14 @@ package com.example.projprogavanc.DB
 
 import android.content.ContentValues
 
-class Game_Store(var preco: Double, var id: Long=-1) {
+class Game_Store(var preco: Double,var game_id :Long, var store_id :Long ,var id: Long=-1) {
 
     fun toContentValues(): ContentValues {
 
         val values = ContentValues()
         values.put(TDBGame_Store.C_PRECO, preco)
+        values.put(TDBGame_Store.C_GAME_ID, game_id)
+        values.put(TDBGame_Store.C_STORE_ID, store_id)
 
         return values
 
