63586a5e216f454adb61a77ed5fcbcea536abdaf
RDA <ricardo.dias.andrade@gmail.com>
2022-06-08 16:53:51 +0100

    added function fromCursor to Game_Store.kt

--- app/src/main/java/com/example/projprogavanc/DB/Game_Store.kt
+++ app/src/main/java/com/example/projprogavanc/DB/Game_Store.kt
@@ -1,6 +1,8 @@
 package com.example.projprogavanc.DB
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 /**
  * Class para item do tipo Game_Store
@@ -21,6 +23,24 @@ data class Game_Store(var preco: Double,var game_id :Long, var store_id :Long, v
         values.put(TDBGame_Store.C_STORE_ID, store_id)
 
         return values
+
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor):Game_Store{
+
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posPrice = cursor.getColumnIndex(TDBGame_Store.C_PRECO)
+            val posGameId = cursor.getColumnIndex(TDBGame_Store.C_GAME_ID)
+            val posStoreId = cursor.getColumnIndex(TDBGame_Store.C_STORE_ID)
+
+            val id = cursor.getLong(posId)
+            val price = cursor.getDouble(posPrice)
+            val gameId = cursor.getLong(posGameId)
+            val storeId = cursor.getLong(posStoreId)
+
+            return Game_Store(price,gameId,storeId,id)
+        }
 
     }
 }
