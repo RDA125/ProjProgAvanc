8f9138fa45ca6760a1af8b12f2b53a2bf6ce5002
RDA <ricardo.dias.andrade@gmail.com>
2022-06-20 15:13:37 +0100

    testing how the layout activity looks and functions Changed gallery interface to show recclerView of a wishlist(Game_Store table values)

--- app/src/main/java/com/example/projprogavanc/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/projprogavanc/ui/gallery/GalleryFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.projprogavanc.databinding.FragmentGalleryBinding
+import com.example.projprogavanc.databinding.FragmentGameStoreListBinding
 
 class GalleryFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentGameStoreListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,15 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        ViewModelProvider(this).get(GalleryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentGameStoreListBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
+        /*val textView: TextView = binding.textGallery
         galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
     }
 
